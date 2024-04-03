9c6fddbf025a54650ea5e9118ed30182e8692657
livingjusttodie <162730543+livingjusttodie@users.noreply.github.com>
2024-04-03 14:29:51 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "auto";
+}
+
+function normalImg(x) {
+  x.style.height = "128px";
+  x.style.width = "auto";
+}// JavaScript Document
